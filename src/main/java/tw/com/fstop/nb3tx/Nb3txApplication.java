69f44e4c49d2ac07dad30a38c35e4a3dc02c7c23
guansheng <guanshengli@fstop.com.tw>
2026-01-29 09:59:23 +0800

    Initial commit

---
+++ src/main/java/tw/com/fstop/nb3tx/Nb3txApplication.java
@@ -0,0 +1,13 @@
+package tw.com.fstop.nb3tx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nb3txApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nb3txApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/com/fstop/nb3tx/Nb3txApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.fstop.nb3tx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nb3txApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
