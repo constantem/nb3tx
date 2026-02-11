f229f938671e71e84bbc97bcb91ccfea79db3e25
moai1222 <aa0985629526@gmail.com>
2026-02-11 10:22:02 +0800

    tw

--- src/main/java/tw/com/fstop/nb3tx/controller/TwdTransactionController.java
+++ src/main/java/tw/com/fstop/nb3tx/controller/TwdTransactionController.java
@@ -1,6 +1,7 @@
 package tw.com.fstop.nb3tx.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +11,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import tw.com.fstop.nb3tx.repository.BankRepository;
 import tw.com.fstop.nb3tx.repository.ReplyRepository;
-import tw.com.fstop.nb3tx.domain.Bank;           // 引用您的 Bank Domain
+import tw.com.fstop.nb3tx.domain.Bank; // 引用您的 Bank Domain
 
 import java.util.*;
 
